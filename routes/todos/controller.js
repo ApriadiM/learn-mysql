607c9b66cb4bda3f4848e56b19bd06d92cb6eac7
Apriadi <apriadim>
2019-12-03 00:57:51 +0700

    add todo

--- routes/todos/controller.js
+++ routes/todos/controller.js
@@ -1,76 +1,64 @@
-const {connection} = require("../../config")
+const { connection } = require("../../config");
 
 module.exports = {
-getAll: (req, res) => {
-    console.log(req.params.name);
-    
-    connection.query(`select * from todo WHERE name='${req.params.name}'`, (error, results, fields) => {
-        if (error) {
-            res.status(500).send({msg: "have error"})
-        } else {
-            res.status(200).send({
-                msg: "show data",
-                data : results
-            })
-        }
-    })
-},
-addOne: (req, res) => {
-    console.log(req.body);
-    
-    connection.query(
-        `INSERT INTO todo (id, todo, name, email) VALUES (NULL, '${req.body.todo}', '${req.body.name}', '${req.body.email}')`, (error, results, fields) => {
-        if (error) {
-            res.status(500).send({msg: "have error add"})
-        } else {
-            res.status(200).send({
-                msg: "show data",
-                data : results
-            })
-        }
-    })
-},
-
-updateOne: (req, res) => {
-    console.log(req.body);
-    
-    connection.query(
-    //    ` UPDATE todo SET todo = 'makan lagi update again' WHERE todo.id = 1`,
-
-
-        `UPDATE todo SET ? WHERE id = ?`,
-         [req.body, `${req.params.id}`],
-          (error, results, fields) => {
-        if (error) {
-            res.status(500).send({msg: "have error add"})
-        } else {
-            res.status(200).send({
-                msg: "show data",
-                data : results
-            })
-        }
-    })
-},
-deleteOne: (req, res) => {
-    console.log(req.body);
-    
-    connection.query(
-    //    ` UPDATE todo SET todo = 'makan lagi update again' WHERE todo.id = 1`,
-
-
-        `DELETE from todo WHERE id = ?`,
-         [req.params.id],
-          (error, results, fields) => {
-        if (error) {
-            res.status(500).send({msg: "have error add"})
-        } else {
-            res.status(200).send({
-                msg: "show data",
-                data : results
-            })
-        }
-    })
-}
-
-
-}
+    getAll: (req, res) => {
+        connection.query(`Select * From todo WHERE name='${req.params.name}`, (error, results, fields) => {
+            if (error) {
+                res.status(500).send({ message: "there is something error"}
+                );
+            } else {
+                res.status(200).send({ 
+                    message: "Show all datas",
+                    data: results
+                });
+            }
+        });
+    },
+    addOne: (req, res) => {
+        connection.query(
+            `INSERT INTO todo (id, todo, name, email) VALUES (NULL, '${req.body.todo}', '${req.body.name}', '${req.body.email}')`,
+            (error, results, fields) => {
+            if (error) {
+                res.status(500).send({ message: "there is something error"}
+                );
+            } else {
+                res.status(200).send({ 
+                    message: "Add one data",
+                    data: results
+                });
+            }
+        });
+    },
+    updateOne: (req, res) => {
+        connection.query(
+            `UPDATE todo SET ? WHERE id = ?`,
+            [req.body, `${req.params.id}`],
+            (error, results, fields) => {
+            if (error) {
+                res.status(500).send({ message: "there is something error"}
+                );
+            } else {
+                res.status(200).send({ 
+                    message: "Update one data",
+                    data: results
+                });
+            }
+        });
+    },
+    deleteOne: (req, res) => {
+        connection.query(
+            `DELETE FROM todo WHERE id = ?`,
+            [req.params.id],
+            (error, results, fields) => {
+            if (error) {
+                res.status(500).send({ message: "there is something error"}
+                );
+            } else {
+                res.status(200).send({ 
+                    message: "Delete one data",
+                    data: results
+                });
+            }
+        });
+    }
+};
